ca35370d18002afe32cdf54a90b6fa0376db9ba6
blumenthaler <blumenthaleralex@gmail.com>
2021-02-22 14:18:53 -0500

    set port variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 // app.get()
 // app.post()
@@ -21,5 +22,4 @@ app.get('/api/articles', (req, res) => {
     ]})
 })
 
-
-app.listen(3000, () => console.log('Listening on port 3000...'))
+app.listen(port, () => console.log(`Listening on port ${port}...`))
