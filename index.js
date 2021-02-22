3d87d157204155124e97b051c6406bb694a05bee
blumenthaler <blumenthaleralex@gmail.com>
2021-02-22 14:30:24 -0500

    created basic route for retrieving one article

--- index.js
+++ index.js
@@ -8,18 +8,29 @@ const port = process.env.PORT || 3000
 // app.delete()
 // app.listen()
 
+// seed
+const articles = [
+    {id: 1, author: 'Alex B', content: 'This is an article I wrote'},
+    {id: 2, author: 'Martin Scorsese', content: "Another article, about movies and their cultural value"}
+]
+
 // Root
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-// Collection of articles, scraped from Reuters homepage
+// Articles
+// Index: ollection of articles, scraped from Reuters homepage
 app.get('/api/articles', (req, res) => {
-    // for now, dummy data
-    res.send({articles: [
-        {id: 1, author: 'Alex B', content: 'This is an article I wrote'},
-        {id: 2, author: 'Martin Scorsese', content: "Another article, about movies and their cultural value"}
-    ]})
+    res.send({articles})
+})
+
+// Get one article by id
+app.get('/api/articles/:id', (req, res) => {
+    const article = articles.find(article => article.id === parseInt(req.params.id))
+    if (!article) res.status(404).send('Article with given id not found')
+    res.send(article)
+    
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}...`))
