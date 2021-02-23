7333f68aafee6f8e8dfce44053f7bb183498da85
blumenthaler <blumenthaleralex@gmail.com>
2021-02-23 11:49:28 -0500

    successfully outputs article data to console

--- scraper.js
+++ scraper.js
@@ -2,18 +2,34 @@ const rp = require('request-promise');
 const URL = 'https://www.reuters.com/'
 const $ = require('cheerio');
 
+let title
+let content = []
 
 rp(URL)
     .then(html => {
         // gets link for topstory article
         const link = $('h2 > a', html)[1].attribs.href
         const full = URL + link
-        console.log(full)
+        // console.log(full)
         rp (full)
             .then(html => {
                 // get article's headline
-                const headline = $('h1', html)[0].children[0].data
-                console.log(headline)
+                title = $('h1', html)[0].children[0].data
+
+                // get article content
+                const contentParagraphs = $('.Paragraph-paragraph-2Bgue.ArticleBody-para-TD_9x', html)
+                for (const property in contentParagraphs) {
+                    if (contentParagraphs[property].children) {
+                        const firstChild = contentParagraphs[property].children[0]
+                        if (firstChild?.data) {
+                            content.push(firstChild.data)
+                        }
+                    }
+                }
+                let final = content.join('\n')
+                
+                console.log(title)
+                console.log(final)
             })
             .catch(error => {
                 console.log(error)
@@ -22,3 +38,4 @@ rp(URL)
     .catch(error => {
         console.log(error)
     })
+
