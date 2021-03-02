1d96f4ed0df767aa63de3e7d92c579f3e2bd3d08
Alex Blumenthaler <blumenthaleralex@gmail.com>
2021-03-02 13:28:49 -0500

    wrapped scraper in variable function, exported to index
    exported scraper to index

--- scraper.js
+++ scraper.js
@@ -2,40 +2,50 @@ const rp = require('request-promise');
 const URL = 'https://www.reuters.com/'
 const $ = require('cheerio');
 
-let title
+let title, final
 let content = []
 
-rp(URL)
-    .then(html => {
-        // gets link for topstory article
-        const link = $('h2 > a', html)[1].attribs.href
-        const full = URL + link
-        // console.log(full)
-        rp (full)
-            .then(html => {
-                // get article's headline
-                title = $('h1', html)[0].children[0].data
 
-                // get article content
-                const contentParagraphs = $('.Paragraph-paragraph-2Bgue.ArticleBody-para-TD_9x', html)
-                for (const property in contentParagraphs) {
-                    if (contentParagraphs[property].children) {
-                        const firstChild = contentParagraphs[property].children[0]
-                        if (firstChild?.data) {
-                            content.push(firstChild.data)
+const scraper = () => {
+    rp(URL)
+        .then(html => {
+            // gets link for topstory article
+            const link = $('h2 > a', html)[1].attribs.href
+            const full = URL + link
+            // console.log(full)
+            rp (full)
+                .then(html => {
+                    // get article's headline
+                    title = $('h1', html)[0].children[0].data
+
+                    // get article content
+                    const contentParagraphs = $('.Paragraph-paragraph-2Bgue.ArticleBody-para-TD_9x', html)
+                    for (const property in contentParagraphs) {
+                        if (contentParagraphs[property].children) {
+                            const firstChild = contentParagraphs[property].children[0]
+                            if (firstChild?.data) {
+                                content.push(firstChild.data)
+                            }
                         }
                     }
-                }
-                let final = content.join('\n')
-                
-                console.log(title)
-                console.log(final)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    })
-    .catch(error => {
-        console.log(error)
-    })
+                    final = content.join('\n')
+                    // add article to database
+                    // const sendable = {
+                    //     id: 3,
+                    //     title,
+                    //     content: final
+                    // }
+                    console.log(title)
+                    console.log(final)
+                 
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
 
+exports.scraper = scraper
