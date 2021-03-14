cd96490ad56fd89ef119caeb642c5e5f872a6c8f
Alex Blumenthaler <blumenthaleralex@gmail.com>
2021-03-14 18:27:03 -0400

    Successfully scrapes top Technology story, adds to API

--- topTechScraper.js
+++ topTechScraper.js
@@ -9,67 +9,58 @@ let title, final
 let content = []
 
 
-const topStoryScraper = () => {
+const topTechScraper = () => {
     rp(REUTERS_URL)
         .then(html => {
-            // gets link for topstory article
+            // gets link for topstory tech article
             const link = $('.story-content > a', html)[0].attribs.href
             const full = REUTERS_URL + link
             console.log(link)
-        
-//             // console.log(full)
             rp (full)
                 .then(html => {
                     // get article's headline
                     title = $('h1', html)[0].children[0].data
-                    console.log(title)
-            
-            
+                   // get article content
+                    const contentParagraphs = $('p', html)
            
-
-//                   // get article content
-                    const contentParagraphs = $('.Paragraph-paragraph-2Bgue.ArticleBody-para-TD_9x', html)
-                    console.log(contentParagraphs)
+           
+                    for (const property in contentParagraphs) {
+                        if (contentParagraphs[property].children) {
+                            const firstChild = contentParagraphs[property].children[0]
+                            if (firstChild?.data) {
+                                content.push(firstChild.data)
+                            }
+                        }
+                    }
+                    final = content.join('\n')
+                      // add article to database
+                    const sendable = {
+                        title,
+                        content: final
+                    }
+                    console.log(title)
+                    console.log(final)
+                    sendTopStory(sendable)
                 })
-        })
-//                     for (const property in contentParagraphs) {
-//                         if (contentParagraphs[property].children) {
-//                             const firstChild = contentParagraphs[property].children[0]
-//                             if (firstChild?.data) {
-//                                 content.push(firstChild.data)
-//                             }
-//                         }
-//                     }
-//                     final = content.join('\n')
-//                     // add article to database
-//                     const sendable = {
-//                         title,
-//                         content: final
-//                     }
-//                     console.log(title)
-//                     console.log(final)
-//                     sendTopStory(sendable)
-                 
-//                 })
-//                 .catch(error => {
-//                     console.log(error)
-//                 })
-//         })
-//         .catch(error => {
-//             console.log(error)
-//         })
-// }
-
-// const sendTopStory = articleData => {
-//     return fetch(API_URL + "api/articles", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify(articleData)
-//     }).then(resp => resp.json())
-//     .then(data => console.log("Success: " + data))
-//     .catch(error => console.log(error))
+                .catch(error => {
+                    console.log(error)
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
 }
 
-topStoryScraper()
+const sendTopStory = articleData => {
+    return fetch(API_URL + "api/articles", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(articleData)
+    }).then(resp => resp.json())
+    .then(data => console.log("Success: " + data))
+    .catch(error => console.log(error))
+}
 
-exports.topStoryScraper = topStoryScraper
+topTechScraper()
+
+exports.topTechScraper = topTechScraper
